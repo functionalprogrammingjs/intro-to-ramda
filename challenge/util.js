a620c7b55e1f7ae758dca5106e9e16884a22a5d7
Álvaro Cuesta <alvaro@redradix.com>
2019-04-24 16:26:32 +0200

    add missing types to util.js

--- challenge/util.js
+++ challenge/util.js
@@ -9,12 +9,16 @@ const {
   length,
 } = require('ramda')
 
+// fold :: (b -> a -> b) -> [a] -> b
 const fold = curry((fn, array) => array.reduce(fn))
 
+// arrayMaxBy :: (a -> b) -> [a] -> a
 const arrayMaxBy = useWith(fold, [maxBy])
 
+// arrayMinBy :: (a -> b) -> [a] -> a
 const arrayMinBy = useWith(fold, [minBy])
 
+// average :: [num] -> num
 const average = converge(divide, [sum, length])
 
 module.exports = {
