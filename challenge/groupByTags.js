50b81066d11b5f1c82f3053261f0a2198a01f93a
Álvaro Cuesta <alvaro@redradix.com>
2019-04-24 10:28:59 +0200

    simplify groupByTags to show converge function

--- challenge/groupByTags.js
+++ challenge/groupByTags.js
@@ -1,25 +1,26 @@
 const {
   __,
+  compose,
   prop,
   converge,
-  identity,
+  assoc,
   chain,
   map,
-  assoc,
-  pipe,
-  groupBy
-} = require('../lib')
+  omit,
+  groupBy,
+} = require('ramda')
 
-// postsByTags :: (post, [tags]) -> [posts]
-const postsByTags = (post, tags) => map(assoc('tag', __, post), tags)
+// repeatWithEachTag :: post -> [posts]
+const repeatWithEachTag = converge(
+  map,
+  [post => assoc('tag', __, post), prop('tags')],
+)
 
-// postsWithTag :: post -> [posts]
-const postsWithTag = converge(postsByTags, [identity, prop('tags')])
-
-// groupByTags :: [post] -> { [tag]: [post]}
-const groupByTags = pipe(
-  chain(postsWithTag),
-  groupBy(prop('tag'))
+// groupByTags :: [post] -> { [tag]: [post] }
+const groupByTags = compose(
+  map(map(omit(['tag']))),
+  groupBy(prop('tag')),
+  chain(repeatWithEachTag),
 )
 
 module.exports = groupByTags
