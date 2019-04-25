0958e14bd928d6f45f5d6503c19d17eb90dc85d6
Álvaro Cuesta <alvaro@redradix.com>
2019-04-25 15:02:11 +0200

    simplify groupByTags

--- challenge/groupByTags.js
+++ challenge/groupByTags.js
@@ -10,11 +10,11 @@ const {
   groupBy,
 } = require('ramda')
 
+// postsByTags :: (post, [tags]) -> [posts]
+const postsByTags = (post, tags) => map(assoc('tag', __, post), tags)
+
 // repeatWithEachTag :: post -> [posts]
-const repeatWithEachTag = converge(
-  map,
-  [post => assoc('tag', __, post), prop('tags')],
-)
+const repeatWithEachTag = converge(postsByTags, [identity, prop('tags')])
 
 // groupByTags :: [post] -> { [tag]: [post] }
 const groupByTags = compose(
